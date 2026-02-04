3e48c8d24705d2aaa0ea8885256c256fbb023352
hoho <lche417@aucklanduni.ac.nz>
2026-02-04 13:51:56 +1300

    finish lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes() {
+        IntList lst = IntList.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 19, 20);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 4 -> 25 -> 6 -> 49 -> 8 -> 9 -> 10 -> 121 -> 12 -> 169 -> 361 -> 20", lst.toString());
+        assertTrue(changed);
+    }
 }
